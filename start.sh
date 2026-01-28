9828f3ac037141de8496c452f54d62ae92466301
freeradical16 <freeradical16@gmail.com>
2026-01-28 10:26:37 -0600

    Use Supervisor to run ComfyUI and JupyterLab
    Replaced JupyterLab startup commands with Supervisor to manage processes.

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-# JupyterLab
-jupyter lab \
-  --ip 0.0.0.0 --port 8888 \
-  --no-browser --allow-root \
-  --ServerApp.token="${JUPYTER_TOKEN:-}" \
-  --ServerApp.password='' &
-
-# ComfyUI
-cd /workspace/ComfyUI
-python3 main.py --listen 0.0.0.0 --port 8188
+# Supervisor runs ComfyUI + JupyterLab + FileBrowser
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
